a28766e45459a658f37628080001816213f93e70
wborgsm <wborgsm@localhost>
2015-03-13 19:03:56 +0000

    Fixed CfxWebPluginUnstableCallback as callback event container.

--- ChromiumFX/Generated/CfxWebPluginUnstableCallback.cs
+++ ChromiumFX/Generated/CfxWebPluginUnstableCallback.cs
@@ -34,6 +34,8 @@
 using System;
 
 namespace Chromium {
+    using Event;
+
     /// <summary>
     /// Structure to implement for receiving unstable plugin information. The
     /// functions of this structure will be called on the browser process IO thread.
@@ -44,27 +46,37 @@ namespace Chromium {
     /// </remarks>
     public class CfxWebPluginUnstableCallback : CfxBase {
 
-        private static readonly WeakCache weakCache = new WeakCache();
-
         internal static CfxWebPluginUnstableCallback Wrap(IntPtr nativePtr) {
             if(nativePtr == IntPtr.Zero) return null;
-            lock(weakCache) {
-                var wrapper = (CfxWebPluginUnstableCallback)weakCache.Get(nativePtr);
-                if(wrapper == null) {
-                    wrapper = new CfxWebPluginUnstableCallback(nativePtr);
-                    weakCache.Add(wrapper);
-                } else {
-                    CfxApi.cfx_release(nativePtr);
-                }
-                return wrapper;
-            }
+            var handlePtr = CfxApi.cfx_web_plugin_unstable_callback_get_gc_handle(nativePtr);
+            return (CfxWebPluginUnstableCallback)System.Runtime.InteropServices.GCHandle.FromIntPtr(handlePtr).Target;
         }
 
 
+        private static object eventLock = new object();
+
+        // is_unstable
+        [System.Runtime.InteropServices.UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.StdCall, SetLastError = false)]
+        private delegate void cfx_web_plugin_unstable_callback_is_unstable_delegate(IntPtr gcHandlePtr, IntPtr path_str, int path_length, int unstable);
+        private static cfx_web_plugin_unstable_callback_is_unstable_delegate cfx_web_plugin_unstable_callback_is_unstable;
+        private static IntPtr cfx_web_plugin_unstable_callback_is_unstable_ptr;
+
+        internal static void is_unstable(IntPtr gcHandlePtr, IntPtr path_str, int path_length, int unstable) {
+            var self = (CfxWebPluginUnstableCallback)System.Runtime.InteropServices.GCHandle.FromIntPtr(gcHandlePtr).Target;
+            if(self == null) {
+                return;
+            }
+            var e = new CfxWebPluginUnstableCallbackIsUnstableEventArgs(path_str, path_length, unstable);
+            var eventHandler = self.m_IsUnstable;
+            if(eventHandler != null) eventHandler(self, e);
+            e.m_isInvalid = true;
+        }
+
         internal CfxWebPluginUnstableCallback(IntPtr nativePtr) : base(nativePtr) {}
+        public CfxWebPluginUnstableCallback() : base(CfxApi.cfx_web_plugin_unstable_callback_ctor) {}
 
         /// <summary>
-        /// Method that will be called for the requested plugin. |unstable| will be
+        /// Method that will be called for the requested plugin. |Unstable| will be
         /// true (1) if the plugin has reached the crash count threshold of 3 times in
         /// 120 seconds.
         /// </summary>
@@ -72,15 +84,94 @@ namespace Chromium {
         /// See also the original CEF documentation in
         /// <see href="https://bitbucket.org/wborgsm/chromiumfx/src/tip/cef/include/capi/cef_web_plugin_capi.h">cef/include/capi/cef_web_plugin_capi.h</see>.
         /// </remarks>
-        public void IsUnstable(string path, bool unstable) {
-            var path_pinned = new PinnedString(path);
-            CfxApi.cfx_web_plugin_unstable_callback_is_unstable(NativePtr, path_pinned.Obj.PinnedPtr, path_pinned.Length, unstable ? 1 : 0);
-            path_pinned.Obj.Free();
+        public event CfxWebPluginUnstableCallbackIsUnstableEventHandler IsUnstable {
+            add {
+                lock(eventLock) {
+                    if(m_IsUnstable == null) {
+                        if(cfx_web_plugin_unstable_callback_is_unstable == null) {
+                            cfx_web_plugin_unstable_callback_is_unstable = is_unstable;
+                            cfx_web_plugin_unstable_callback_is_unstable_ptr = System.Runtime.InteropServices.Marshal.GetFunctionPointerForDelegate(cfx_web_plugin_unstable_callback_is_unstable);
+                        }
+                        CfxApi.cfx_web_plugin_unstable_callback_set_managed_callback(NativePtr, 0, cfx_web_plugin_unstable_callback_is_unstable_ptr);
+                    }
+                    m_IsUnstable += value;
+                }
+            }
+            remove {
+                lock(eventLock) {
+                    m_IsUnstable -= value;
+                    if(m_IsUnstable == null) {
+                        CfxApi.cfx_web_plugin_unstable_callback_set_managed_callback(NativePtr, 0, IntPtr.Zero);
+                    }
+                }
+            }
         }
 
+        private CfxWebPluginUnstableCallbackIsUnstableEventHandler m_IsUnstable;
+
         internal override void OnDispose(IntPtr nativePtr) {
-            weakCache.Remove(nativePtr);
+            if(m_IsUnstable != null) {
+                m_IsUnstable = null;
+                CfxApi.cfx_web_plugin_unstable_callback_set_managed_callback(NativePtr, 0, IntPtr.Zero);
+            }
             base.OnDispose(nativePtr);
         }
+    }
+
+
+    namespace Event {
+
+        /// <summary>
+        /// Method that will be called for the requested plugin. |Unstable| will be
+        /// true (1) if the plugin has reached the crash count threshold of 3 times in
+        /// 120 seconds.
+        /// </summary>
+        /// <remarks>
+        /// See also the original CEF documentation in
+        /// <see href="https://bitbucket.org/wborgsm/chromiumfx/src/tip/cef/include/capi/cef_web_plugin_capi.h">cef/include/capi/cef_web_plugin_capi.h</see>.
+        /// </remarks>
+        public delegate void CfxWebPluginUnstableCallbackIsUnstableEventHandler(object sender, CfxWebPluginUnstableCallbackIsUnstableEventArgs e);
+
+        /// <summary>
+        /// Method that will be called for the requested plugin. |Unstable| will be
+        /// true (1) if the plugin has reached the crash count threshold of 3 times in
+        /// 120 seconds.
+        /// </summary>
+        /// <remarks>
+        /// See also the original CEF documentation in
+        /// <see href="https://bitbucket.org/wborgsm/chromiumfx/src/tip/cef/include/capi/cef_web_plugin_capi.h">cef/include/capi/cef_web_plugin_capi.h</see>.
+        /// </remarks>
+        public class CfxWebPluginUnstableCallbackIsUnstableEventArgs : CfxEventArgs {
+
+            internal IntPtr m_path_str;
+            internal int m_path_length;
+            internal string m_path;
+            internal int m_unstable;
+
+            internal CfxWebPluginUnstableCallbackIsUnstableEventArgs(IntPtr path_str, int path_length, int unstable) {
+                m_path_str = path_str;
+                m_path_length = path_length;
+                m_unstable = unstable;
+            }
+
+            public string Path {
+                get {
+                    CheckAccess();
+                    if(m_path == null && m_path_str != IntPtr.Zero) m_path = System.Runtime.InteropServices.Marshal.PtrToStringUni(m_path_str, m_path_length);
+                    return m_path;
+                }
+            }
+            public bool Unstable {
+                get {
+                    CheckAccess();
+                    return 0 != m_unstable;
+                }
+            }
+
+            public override string ToString() {
+                return String.Format("Path={{{0}}}, Unstable={{{1}}}", Path, Unstable);
+            }
+        }
+
     }
 }
--- libcfx/Generated/cfx_web_plugin_unstable_callback.c
+++ libcfx/Generated/cfx_web_plugin_unstable_callback.c
@@ -37,14 +37,59 @@
 extern "C" {
 #endif
 
-// cef_base_t base
+typedef struct _cfx_web_plugin_unstable_callback_t {
+    cef_web_plugin_unstable_callback_t cef_web_plugin_unstable_callback;
+    unsigned int ref_count;
+    gc_handle_t gc_handle;
+} cfx_web_plugin_unstable_callback_t;
 
-// is_unstable
-CFX_EXPORT void cfx_web_plugin_unstable_callback_is_unstable(cef_web_plugin_unstable_callback_t* self, char16 *path_str, int path_length, int unstable) {
-    cef_string_t path = { path_str, path_length, 0 };
-    self->is_unstable(self, &path, unstable);
+void CEF_CALLBACK _cfx_web_plugin_unstable_callback_add_ref(struct _cef_base_t* base) {
+    cfx_web_plugin_unstable_callback_t* ptr = (cfx_web_plugin_unstable_callback_t*)base;
+    InterlockedIncrement(&ptr->ref_count);
+}
+int CEF_CALLBACK _cfx_web_plugin_unstable_callback_release(struct _cef_base_t* base) {
+    cfx_web_plugin_unstable_callback_t* ptr = (cfx_web_plugin_unstable_callback_t*)base;
+    int count = InterlockedDecrement(&((cfx_web_plugin_unstable_callback_t*)ptr)->ref_count);
+    if(!count) {
+        cfx_gc_handle_free(((cfx_web_plugin_unstable_callback_t*)ptr)->gc_handle);
+        free(ptr);
+    }
+    return count;
 }
 
+CFX_EXPORT cfx_web_plugin_unstable_callback_t* cfx_web_plugin_unstable_callback_ctor(gc_handle_t gc_handle) {
+    cfx_web_plugin_unstable_callback_t* ptr = (cfx_web_plugin_unstable_callback_t*)calloc(1, sizeof(cfx_web_plugin_unstable_callback_t));
+    if(!ptr) return 0;
+    ptr->cef_web_plugin_unstable_callback.base.size = sizeof(cef_web_plugin_unstable_callback_t);
+    ptr->cef_web_plugin_unstable_callback.base.add_ref = _cfx_web_plugin_unstable_callback_add_ref;
+    ptr->cef_web_plugin_unstable_callback.base.release = _cfx_web_plugin_unstable_callback_release;
+    ptr->ref_count = 1;
+    ptr->gc_handle = gc_handle;
+    return ptr;
+}
+
+CFX_EXPORT gc_handle_t cfx_web_plugin_unstable_callback_get_gc_handle(cfx_web_plugin_unstable_callback_t* self) {
+    return self->gc_handle;
+}
+
+// is_unstable
+
+void (CEF_CALLBACK *cfx_web_plugin_unstable_callback_is_unstable_callback)(gc_handle_t self, char16 *path_str, int path_length, int unstable);
+
+void CEF_CALLBACK cfx_web_plugin_unstable_callback_is_unstable(cef_web_plugin_unstable_callback_t* self, const cef_string_t* path, int unstable) {
+    cfx_web_plugin_unstable_callback_is_unstable_callback(((cfx_web_plugin_unstable_callback_t*)self)->gc_handle, path ? path->str : 0, path ? path->length : 0, unstable);
+}
+
+
+CFX_EXPORT void cfx_web_plugin_unstable_callback_set_managed_callback(cef_web_plugin_unstable_callback_t* self, int index, void* callback) {
+    switch(index) {
+    case 0:
+        if(callback && !cfx_web_plugin_unstable_callback_is_unstable_callback)
+            cfx_web_plugin_unstable_callback_is_unstable_callback = (void (CEF_CALLBACK *)(gc_handle_t self, char16 *path_str, int path_length, int unstable)) callback;
+        self->is_unstable = callback ? cfx_web_plugin_unstable_callback_is_unstable : 0;
+        break;
+    }
+}
 
 #ifdef __cplusplus
 } // extern "C"
