5dceff5bbc7e475875bbd07fb91238a755dfe395
wborgsm <wborgsm@localhost>
2015-03-23 09:53:38 +0000

    ChromiumFX 3.2171.10

--- ChromiumFX/Properties/AssemblyInfo.cs
+++ ChromiumFX/Properties/AssemblyInfo.cs
@@ -39,5 +39,4 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.2171.1979.9")]
-[assembly: AssemblyFileVersion("3.2171.1979.9")]
+[assembly: AssemblyVersion("3.2171.10")]
