8ec5416bce1b9a48a78e0872fb18bc4a648078e1
wborgsm <wborgsm@localhost>
2015-04-03 13:41:11 +0100

    Updated version info and readme.

--- ChromiumFX/Properties/AssemblyInfo.cs
+++ ChromiumFX/Properties/AssemblyInfo.cs
@@ -39,4 +39,4 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.2171.11")]
+[assembly: AssemblyVersion("3.2171.12")]
--- ChromiumWebBrowser/Properties/AssemblyInfo.cs
+++ ChromiumWebBrowser/Properties/AssemblyInfo.cs
@@ -40,4 +40,4 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.2171.11")]
+[assembly: AssemblyVersion("3.2171.12")]
