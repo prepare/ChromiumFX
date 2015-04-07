78bda0fd074f8bf523d4169f7305bfbaf9d33deb
wborgsm <wborgsm@localhost>
2015-04-07 10:00:04 +0100

    Create cef branch 1750.

--- cef/include/cef_display_handler.h
+++ cef/include/cef_display_handler.h
@@ -77,8 +77,9 @@ class CefDisplayHandler : public virtual CefBase {
                          CefString& text) { return false; }
 
   ///
-  // Called when the browser receives a status message. |value| contains the
-  // text that will be displayed in the status message.
+  // Called when the browser receives a status message. |text| contains the text
+  // that will be displayed in the status message and |type| indicates the
+  // status message type.
   ///
   /*--cef(optional_param=value)--*/
   virtual void OnStatusMessage(CefRefPtr<CefBrowser> browser,
