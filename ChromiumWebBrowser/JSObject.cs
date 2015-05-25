6b01ad2a4e1ba344ffc73a2ebe049c672b98d85d
wborgsm <wborgsm@localhost>
2015-05-25 08:56:50 +0100

    Organize usings.

--- ChromiumWebBrowser/JSObject.cs
+++ ChromiumWebBrowser/JSObject.cs
@@ -28,11 +28,7 @@
 // TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 using Chromium.Remote;
 using Chromium.Remote.Event;
 
